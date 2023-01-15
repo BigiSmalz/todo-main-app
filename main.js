e5597f4b568e4e653ec2198a19c01233a973f6c1
sire <uchepibe@gmail.com>
2023-01-15 02:53:31 +0100

    feat:rounded up light/darkmode

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 // change theme
-const themeIcon = document.querySelectorAll('.theme');
-themeIcon.addEventListener('click', () => {
- document.boby.classList.toggle('light')
- if (document.boby.classList.contains('light')) {
-  return themeIcon.src = 'images/icon-moon.svg'
- } else {
-  return themeIcon.src = 'images/icon-moon.svg'
- }
-})
-// Add items
-const itemCount = document.querySelectorAll('.count span')
-const mobCount = document.querySelectorAll('.items-count span')
+const themeIcon = document.getElementById('icon');
+const background = document.getElementById(".desktop-background")
+const screenSize = window.innerWidth
 
-itemCount.innerText =
- mobCount.innerText =
+themeIcon.onclick = function () {
+    document.body.classList.toggle("darkTheme");
+    if (document.body.classList.contains("darkTheme")) {
+        themeIcon.src = "./images/icon-moon.svg"
+        background.src = "./images/bg-desktop-dark.jpg"
+    } else {
+        themeIcon.src = "./images/icon-sun.svg"
+        background.src = "./images/bg-desktop-light.jpg"
+    }
+}
+
+window.addEventListener("resize", (e)=>{
+    if(screenSize < 600){
+        background.src = "./images/bg-mobile-dark.jpg"
+    }else{
+        background.src = "./images/bg-mobile-light.jpg"
+    }
+})
+
+
+// Add items
+// const itemCount = document.querySelectorAll('.count span')
+// const mobCount = document.querySelectorAll('.items-count span')
+
+// itemCount.innerText =
+//  mobCount.innerText =
