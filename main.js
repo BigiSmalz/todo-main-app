800ab91407d56ce685bb16235e207e9e814f420f
olisaemeka <olisaemeka1423@gmail.com>
2023-01-17 01:01:40 +0100

    #updated

--- main.js
+++ main.js
@@ -1,39 +1,25 @@
-// change theme
-<<<<<<< HEAD
-const themeIcon = document.querySelectorAll('.theme');
-themeIcon.addEventListener('click', () => {
- document.boby.classList.toggle('light')
- if (document.body.classList.contains('light')) {
-  return themeIcon.src = 'images/icon-moon.svg'
- } else {
-  return themeIcon.src = 'images/icon-moon.svg'
- }
-})
-// Add items
-const addButton = document.querySelector('.todo-input button');
-const itemInput = document.querySelector('.todo-input')
-=======
+//change theme
 const themeIcon = document.getElementById('icon');
 const background = document.getElementById(".desktop-background")
 const screenSize = window.innerWidth
 
 themeIcon.onclick = function () {
-    document.body.classList.toggle("darkTheme");
-    if (document.body.classList.contains("darkTheme")) {
-        themeIcon.src = "./images/icon-moon.svg"
-        background.src = "./images/bg-desktop-dark.jpg"
-    } else {
-        themeIcon.src = "./images/icon-sun.svg"
-        background.src = "./images/bg-desktop-light.jpg"
-    }
+ document.body.classList.toggle("darkTheme");
+ if (document.body.classList.contains("darkTheme")) {
+  themeIcon.src = "./images/icon-moon.svg"
+
+ } else {
+  themeIcon.src = "./images/icon-sun.svg"
+
+ }
 }
 
-window.addEventListener("resize", (e)=>{
-    if(screenSize < 600){
-        background.src = "./images/bg-mobile-dark.jpg"
-    }else{
-        background.src = "./images/bg-mobile-light.jpg"
-    }
+window.addEventListener("resize", (e) => {
+ if (screenSize < 600) {
+  background.src = "./images/bg-mobile-dark.jpg"
+ } else {
+  background.src = "./images/bg-mobile-light.jpg"
+ }
 })
 
 
@@ -43,4 +29,3 @@ window.addEventListener("resize", (e)=>{
 
 // itemCount.innerText =
 //  mobCount.innerText =
->>>>>>> 0f4cdd1c863469be85c87de5ece35ddd63097053
